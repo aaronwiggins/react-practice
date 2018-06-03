7cbf94a299ffe5fb66d8498b33f016397e671a5c
Aaron Wiggins <apw7177@gmail.com>
2018-06-03 12:37:51 -0400

    add click listener

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ class App extends Component {
       { name: "Cad", age: 20 }
     ]
   }
+  switchNameHandler = () => {
+    console.log('was clicked')
+  }
   render() {
     return (
       <div className="App">
@@ -21,7 +24,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <button>Switch Name</button>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: Racing</Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
