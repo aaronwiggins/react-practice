db8729cf4cfb04cde76b7efa29b320d0e9d9d801
Aaron Wiggins <apw7177@gmail.com>
2018-06-03 23:05:05 -0400

    how to pass function as property to another component. add argument to listener to change state.

--- src/App.js
+++ src/App.js
@@ -12,12 +12,12 @@ class App extends Component {
     ],
     otherState: "some value"
   }
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     // console.log('was clicked')
     // DONT DO THIS this.state.persons[0].name = "Changed"
     this.setState({
       persons : [
-        { name: "Changed", age: 22 },
+        { name: newName, age: 22 },
         { name: "Don", age: 21 },
         { name: "Cad", age: 2 }
       ]
@@ -33,10 +33,21 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        <button onClick={() => this.switchNameHandler("Changed")}>Switch Name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age} 
+        />
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, "changed!")} >
+          My hobbies: Racing
+        </Person>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age} 
+        />
       </div>
     );
     // return React.createElement('div',{className:'App'},React.createElement('h1',null,'working now'))
