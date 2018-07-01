4eb5f6242441d3e62b6a54a433b8a9d63f26eb80
Aaron Wiggins <apw7177@gmail.com>
2018-07-01 19:57:54 -0400

    add a click event inside the new loop. it uses a new function which delets the current element from the state.

--- src/App.js
+++ src/App.js
@@ -13,16 +13,11 @@ class App extends Component {
     otherState: "some value",
     showPersons: false
   }
-  switchNameHandler = (newName) => {
-    // console.log('was clicked')
-    // DONT DO THIS this.state.persons[0].name = "Changed"
-    this.setState({
-      persons : [
-        { name: newName, age: 22 },
-        { name: "Don", age: 21 },
-        { name: "Cad", age: 2 }
-      ]
-    })
+  
+  deletePersonHandler = (personIndex) => {
+    const persons = this.state.persons;
+    persons.splice(personIndex,1);
+    this.setState({persons:persons}) 
   }
 
   nameChangedHandler = (event) => {
@@ -52,8 +47,9 @@ class App extends Component {
     if (this.state.showPersons){
       persons = (
         <div>
-          {this.state.persons.map(person => {
+          {this.state.persons.map((person,index) => {
             return <Person 
+              click={() => this.deletePersonHandler(index)}
               name={person.name} 
               age={person.age} />
           })}
