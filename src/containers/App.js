f5e131d7f8e0abb8dacb41a3d349fc6fd4823db6
Aaron Wiggins <apw7177@gmail.com>
2018-07-18 21:09:39 -0400

    add property to state 'counter' which is mutated using best practice of refering to prevsate as opposed to using this.setstate

--- src/containers/App.js
+++ src/containers/App.js
@@ -18,7 +18,8 @@ class App extends PureComponent {
         { id:"afdsa1",name: "Cad", age: 20 }
       ],
       otherState: "some value",
-      showPersons: false
+      showPersons: false,
+      toggleClicked:0
     }
     console.log('[App.js] Inside Constructor', props)
   }
@@ -82,7 +83,12 @@ componentDidUpdate(){
 
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
-    this.setState({showPersons: !doesShow });
+    this.setState( (prevState,props)=>{
+      return{
+        showPersons: !doesShow, 
+        toggleClicked: prevState.toggleClicked+1 
+      }   
+    });
   }
 
   render() {
