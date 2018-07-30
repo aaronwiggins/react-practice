fae5020aa7e17ba2f4e36a15d4cf1d09ed9e0e3d
Aaron Wiggins <apw7177@gmail.com>
2018-07-29 21:49:16 -0400

    add proptype validator to ensure correct prop type for person component.

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 // import Radium from 'radium';
 import classes from './Person.css';
 import withClass from '../../../hoc/withClass';
@@ -34,5 +35,10 @@ class Person extends Component {
     // }
 
 // }
-
+Person.PropTypes = {
+    click: PropTypes.func,
+    name: PropTypes.string,
+    age: PropTypes.number,
+    changed: PropTypes.func
+}
 export default withClass(Person,classes.Person);
